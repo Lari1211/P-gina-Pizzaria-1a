0c6d10a1ced846d092d1667f44e8c5fdbed8d6a1
Mariana Gonçalves <mariana.goncalves@maristavirtual.org.br>
2023-02-01 16:48:25 -0300

    Arquivos do carrinho adicionados

---
+++ Página pizzaria original/JavaScript/sabores-carrinho/models.js
@@ -0,0 +1,64 @@
+/*
+=========================================================================
+Nome do Projeto: Carrinho de Compras em Javascript
+Finalidade: Cursos de Informática do IFSP campus Caraguatatuba 
+Fonte Base: Projetos do Curso B7Web - https://b7web.com.br/ 
+Autor: Denny Paulista Azevedo Filho
+License: MIT License
+========================================================================= 
+*/
+
+let modelsJson = [
+    {id:1, 
+        name:'Calabresa', 
+        tipo: 'Salgada',
+        img:'img_carrinho/calabresa_salgada.png', 
+        price:[25.00, 35.00, 45.00], 
+        sizes:['30cm', '40cm', '50cm'], 
+        description:'Massa caseira, molhos de tomates orgânicos, calabresa defumada, cebola e queijo mussarela.'},
+    {id:2, 
+        name:'Camarão Cremosos', 
+        tipo: 'Salgada',
+        img:'img_carrinho/camarao-cremoso_salgada.png', 
+        price:[25.00, 35.00, 45.00], 
+        sizes:['30cm', '40cm', '50cm'], 
+        description:'Massa caseira, molhos de tomates orgânicos, calabresa defumada, cebola e queijo mussarela.'},
+    {id:3, 
+        name:'Portuguesa', 
+        tipo: 'Salgada',
+        img:'img_carrinho/portuguesa_salgada.png', 
+        price:[25.00, 35.00, 45.00], 
+        sizes:['30cm', '40cm', '50cm'], 
+        description:'Massa caseira, molhos de tomates orgânicos, calabresa defumada, cebola e queijo mussarela.'},
+    {id:4, 
+        name:'Italianissima', 
+        tipo: 'Salgada',
+        img:'img_carrinho/italianissima.png', 
+        price:[25.00, 35.00, 45.00], 
+        sizes:['30cm', '40cm', '50cm'], 
+        description:'Massa caseira, molho de alcachofra, muçarela de búfala e presunto Parma.'},
+    {id:5, 
+        name:'Milho com bacon', 
+        img:'img_carrinho/milho e bacon.png', 
+        price:[25.00, 35.00, 45.00], 
+        sizes:['30cm', '40cm', '50cm'], 
+        description:'Massa caseira, molhos de tomates orgânicos, calabresa defumada, cebola e queijo mussarela.'},
+    {id:6, 
+        name:'Caprese', 
+        img:'img_carrinho/caprese.png', 
+        price:[25.00, 35.00, 45.00], 
+        sizes:['30cm', '40cm', '50cm'], 
+        description:'Massa caseira, muçarela, muçarela de búfala, rodelas de tomate, folhas de manjericão e pesto de azeitona.'},
+    {id:7, 
+        name:'Carne seca', 
+        img:'img_carrinho/carne-seca.png', 
+        price:[25.00, 35.00, 45.00], 
+        sizes:['30cm', '40cm', '50cm'], 
+        description:'Massa caseira, molhos de tomates orgânicos, calabresa defumada, cebola e queijo mussarela.'},
+    {id:8, 
+        name:' Shitakerita', 
+        img:'img_carrinho/Shitakerita.png', 
+        price:[25.00, 35.00, 45.00], 
+        sizes:['30cm', '40cm', '50cm'], 
+        description:'Massa caseira, shitake, shimeji, mussarela, rodelas de tomates, folhas de manjericão e um toque de parmessão.'},     
+];
